d168fc3ec24b3601bb7d5b24727afc2daea74d33
Patrice Codes <42042567+patrice111@users.noreply.github.com>
2021-02-13 13:10:01 -0500

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,4 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+//const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
