098590f4d79899a14cf9e1e2ea39e6ad07055a93
Patrice Codes <42042567+patrice111@users.noreply.github.com>
2021-02-19 19:31:38 -0500

    Add files via upload

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,7 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
+
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
@@ -44,6 +45,6 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
     ]
 }
