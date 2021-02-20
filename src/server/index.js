644b5fe00d385ab662745e842ff21b297cf6e03a
Patrice Codes <42042567+patrice111@users.noreply.github.com>
2021-02-19 19:50:47 -0500

    Add files via upload

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
+const bodyParser = require('body-parser')
+const cors = require('cors');
 const app = express()
 
+app.use(express.static('dist'))
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 app.use(express.static('dist'))
 
 console.log(__dirname)
@@ -21,3 +30,13 @@ app.listen(8081, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+// MeaningCloud API
+var textapi = new MeaningCloud({
+    /*
+    application_id: "https://api.meaningcloud.com/sentiment-2.1?",
+    application_key: "4b3ca4bda944456e99daed73d3b30439"
+    */
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+  });
+  console.log(`Your API key is ${process.env.API_KEY}`);
