a21a5c12a101a22177f082e0128cc6bfd3e27f3c
Patrice Codes <42042567+patrice111@users.noreply.github.com>
2021-02-22 17:24:42 -0500

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -15,4 +15,23 @@ function handleSubmit(event) {
     })
 }
 
+fetch('http://localhost:8081/article', {
+            method: "POST",
+            credentials: "same-origin",
+            headers: {
+            "Content-Type": "application/json"
+            },
+            body: JSON.stringify({url: formText}),
+        })
+        .then(res => {
+            return res.json()
+        })
+        .then(function(res) {
+            document.getElementById('score_tag').innerHTML =res.score_tag
+            document.getElementById("positive").innerHTML = `Positive: ${res.positive}`;
+            document.getElementById("neutral").innerHTML = `Neutral: ${res.neutral}`;
+            document.getElementById("negative").innerHTML = `Negative: ${res.negative}`;
+            document.getElementById("without_sentiment").innerHTML = `Without Sentiment: ${res.without_sentiment}`;
+        })
+
 export { handleSubmit }
