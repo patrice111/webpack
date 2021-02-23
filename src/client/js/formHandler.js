4af7fd7b38d04f01ba2fe450a8e1d06204b8f3b0
Patrice Codes <42042567+patrice111@users.noreply.github.com>
2021-02-22 22:33:06 -0500

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,20 +1,27 @@
 function handleSubmit(event) {
     event.preventDefault()
-
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let formText = document.getElementById('url').value
     Client.checkForName(formText)
-
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch('http://localhost:8081/article', {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify({url: formText}),
+    })
     .then(res => {
         return res.json()
     })
-    .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
+    .then(function(res) {
+        document.getElementById('Positive').innerHTML =res.score_tag
+        // put remaining element
     })
 }
 
+/*
 fetch('http://localhost:8081/article', {
             method: "POST",
             credentials: "same-origin",
@@ -34,4 +41,5 @@ fetch('http://localhost:8081/article', {
             document.getElementById("without_sentiment").innerHTML = `Without Sentiment: ${res.without_sentiment}`;
         })
 
+*/        
 export { handleSubmit }
