f2599f190fccb5707bf1c0b99d16061cf09f63d0
Patrice Codes <42042567+patrice111@users.noreply.github.com>
2021-02-23 10:36:12 -0500

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,8 +2,10 @@ function handleSubmit(event) {
     event.preventDefault()
     // check what text was put into the form field
     let formText = document.getElementById('url').value
-    Client.checkForName(formText)
+    if(checkUrl(formText))
     console.log("::: Form Submitted :::")
+
+    
     fetch('http://localhost:8081/article', {
         method: "POST",
         credentials: "same-origin",
@@ -17,29 +19,12 @@ function handleSubmit(event) {
     })
     .then(function(res) {
         document.getElementById('Positive').innerHTML =res.score_tag
-        // put remaining element
+        document.getElementById("positive").innerHTML = `Positive: ${res.positive}`;
+        document.getElementById("neutral").innerHTML = `Neutral: ${res.neutral}`;
+        document.getElementById("negative").innerHTML = `Negative: ${res.negative}`;
+        document.getElementById("without_sentiment").innerHTML = `Without Sentiment: ${res.without_sentiment}`;
     })
 }
 
-/*
-fetch('http://localhost:8081/article', {
-            method: "POST",
-            credentials: "same-origin",
-            headers: {
-            "Content-Type": "application/json"
-            },
-            body: JSON.stringify({url: formText}),
-        })
-        .then(res => {
-            return res.json()
-        })
-        .then(function(res) {
-            document.getElementById('score_tag').innerHTML =res.score_tag
-            document.getElementById("positive").innerHTML = `Positive: ${res.positive}`;
-            document.getElementById("neutral").innerHTML = `Neutral: ${res.neutral}`;
-            document.getElementById("negative").innerHTML = `Negative: ${res.negative}`;
-            document.getElementById("without_sentiment").innerHTML = `Without Sentiment: ${res.without_sentiment}`;
-        })
-
-*/        
+    
 export { handleSubmit }
