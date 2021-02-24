d485befb69b5f8735e67177d152bb663734c8f04
Patrice Codes <42042567+patrice111@users.noreply.github.com>
2021-02-23 21:54:50 -0500

    Add files via upload

--- __test__/articleEval.test.js
+++ __test__/articleEval.test.js
@@ -1,5 +1,5 @@
 // Import the js file to test
-import {  } from "../src/client/js/formHandler"
+import { handleSubmit } from "../src/client/js/formHandler"
 
 // The describe() function takes two arguments - a string description, and a test suite as a callback function.  
 // A test suite may contain one or more related tests    
