b0aa656510b55237e940e4d867859e60993ff48c
Patrice Codes <42042567+patrice111@users.noreply.github.com>
2021-02-23 21:52:36 -0500

    Add files via upload

--- src/server/index.js
+++ src/server/index.js
@@ -24,7 +24,7 @@ console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile(path.resolve('src/dist/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
